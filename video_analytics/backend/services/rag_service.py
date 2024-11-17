4c5e2baaf7b3752af98fdd72301969f306030116
paravindranath <deepu.r@gmail.com>
2024-11-16 21:10:09 -0800

    refactor: Update text splitter import from community to text_splitters package

--- video_analytics/backend/services/rag_service.py
+++ video_analytics/backend/services/rag_service.py
@@ -1,4 +1,4 @@
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import JSONLoader
