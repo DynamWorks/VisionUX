02b126587b3b58565e5d6501a2197ad8db26996b
paravindranath <deepu.r@gmail.com>
2024-11-18 12:44:58 -0800

    refactor: Update Rerun iframe source with version and improved welcome screen parameter

--- frontend/src/components/RerunViewer.js
+++ frontend/src/components/RerunViewer.js
@@ -10,8 +10,8 @@ const RerunViewer = () => {
             borderRadius: '8px',
             overflow: 'hidden'
         }}>
-            <iframe
-                src={`https://app.rerun.io/web-viewer/latest/?ws_url=${process.env.REACT_APP_RERUN_WS_URL}&skip_welcome=true`}
+            <iframe // the src should not change :src={`https://app.rerun.io/version/${process.env.REACT_APP_RERUN_VER}/?ws_url=${process.env.REACT_APP_RERUN_WS_URL}&hide_welcome_screen=true`}
+                src={`https://app.rerun.io/version/${process.env.REACT_APP_RERUN_VER}/?ws_url=${process.env.REACT_APP_RERUN_WS_URL}&hide_welcome_screen=true`}
                 style={{
                     width: '100%',
                     height: '100%',
