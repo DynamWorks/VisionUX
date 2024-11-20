b430ba250bfab654c62b48ac14f54277e6db075f
paravindranath <deepu.r@gmail.com>
2024-11-19 18:24:53 -0800

    refactor: Replace hardcoded localhost URLs with environment variables

--- frontend/src/components/RerunViewer.js
+++ frontend/src/components/RerunViewer.js
@@ -11,7 +11,7 @@ const RerunViewer = () => {
             overflow: 'hidden'
         }}>
             <iframe
-                src={`http://localhost:9090?url=ws://localhost:4321`}
+                src={`${process.env.REACT_APP_RERUN_URL}?url=${process.env.REACT_APP_RERUN_WS_URL}`}
                 style={{
                     width: '100%',
                     height: '100%',
