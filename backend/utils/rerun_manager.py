52de3efa4d5414a0651444d1d63efbee190569e1
paravindranath <deepu.r@gmail.com>
2024-11-21 18:28:23 -0800

    fix: Add debug breakpoint in rerun_manager for troubleshooting

--- backend/utils/rerun_manager.py
+++ backend/utils/rerun_manager.py
@@ -149,6 +149,7 @@ class RerunManager:
             self.logger.info("Created new Rerun recording")
                 
             # Load blueprint configuration from config
+            import pdb; pdb.set_trace()
             blueprint_config = self._config.get('rerun', {}).get('blueprint', {})
             views = []
             
