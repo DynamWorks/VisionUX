8b6ac93b8e1adff6e6a3a3747f4b5c6decd69c02
paravindranath <deepu.r@gmail.com>
2024-11-21 21:34:33 -0800

    fix: Update RerunViewer to pass WebSocket URL as query parameter

--- frontend/src/components/RerunViewer.js
+++ frontend/src/components/RerunViewer.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Box } from '@mui/material';
 
 const RerunViewer = () => {
-    const rerunWebUrl = process.env.REACT_APP_RERUN_WEB_URL || 'http://localhost:9090';
+    const rerunWebUrl = `${process.env.REACT_APP_RERUN_WEB_URL}?url=${encodeURIComponent(process.env.REACT_APP_RERUN_WS_URL)}'
+        // process.env.REACT_APP_RERUN_WEB_URL || 'http://localhost:9090';
 
     return (
         <Box sx={{
@@ -13,7 +14,7 @@ const RerunViewer = () => {
             overflow: 'hidden'
         }}>
             <iframe
-                src={`${rerunWebUrl}`}
+                src=${rerunWebUrl}
                 style={{
                     width: '100%',
                     height: '100%',
