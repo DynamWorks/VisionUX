88fb67e31460c088f7fee40a8cfd285e393b066c
paravindranath <deepu.r@gmail.com>
2024-11-29 11:09:46 -0800

    fix: Update environment variable prefix from 'DXV_' to 'DW'

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -15,7 +15,7 @@ class Config:
             config_path: Optional path to YAML config file
         """
         self._config = {}
-        self._env_prefix = 'DXV_'
+        self._env_prefix = 'DW'
         
         # Try to find config file in standard locations
         if not config_path:
