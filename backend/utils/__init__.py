baec15c024d0b5c3265a5bc18c7ec5c0033b19f0
paravindranath <deepu.r@gmail.com>
2024-11-30 21:53:32 -0800

    refactor: Reorganize video streaming utilities with new module structure

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,3 +1,5 @@
 """Utils Package"""
 
-from .video_stream import VideoStream
+from .video_streaming.stream_manager import StreamManager
+from .video_streaming.websocket_publisher import WebSocketPublisher
+from .video_streaming.cv_subscribers import EdgeDetectionSubscriber, MotionDetectionSubscriber, ObjectDetectionSubscriber
